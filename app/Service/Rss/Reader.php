18781c79cfc0252a876276700606bc6f57c0a58f
William <work@suppo.fr>
2024-04-10 22:06:33 +0200

    feat: Create service Reader for fetching feed entries

--- app/Service/Rss/Reader.php
+++ app/Service/Rss/Reader.php
@@ -3,24 +3,45 @@
 namespace App\Service\Rss;
 
 use App\Models\Feed;
+use FeedIo\FeedIo;
 
 class Reader
 {
+    public function __construct(protected FeedIo $client)
+    {
+    }
+
     /**
      * @param \App\Models\Feed $feed
      * @return \App\Service\Rss\PostDto[]
      */
     public function fetch(Feed $feed): array
     {
-        return [
-            new PostDto(
-                title: "Ceci est un test",
-                authorName: "John Doe",
-                url: "https://laravel-france.com",
-                thumbnail: "https://example.org/image.jpg",
-                description: "qsdqs",
-                publishedAt: new \DateTime(),
-            )
-        ];
+        $result = $this->client->read($feed->url);
+
+        $posts = [];
+
+        foreach($result->getFeed() as $item) {
+            $thumbnail = null;
+            $description = null;
+
+            foreach ($item->getMedias() as $media) {
+                if ($media->getThumbnail()) {
+                    $thumbnail = $media->getThumbnail();
+                    $description = $media->getDescription();
+                }
+            }
+
+            $posts[] = new PostDto(
+                title: $item->getTitle(),
+                authorName: $result->getFeed()->getTitle(),
+                url: $item->getLink(),
+                thumbnail: $thumbnail,
+                description: $item->getContent() ?? $description,
+                publishedAt: $item->getLastModified(),
+            );
+        }
+
+        return $posts;
     }
 }
